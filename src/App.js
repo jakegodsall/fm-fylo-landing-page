795e5b7c7ab61c98a04edec848eca242fff9d831
jakegodsall <jake.edward.godsall@gmail.com>
2022-09-09 18:45:30 +0100

    build hero section desktop layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
+import "./App.css";
+
 const App = () => {
     return (
-        <>
+        <div className='main-container'>
             <Navbar />
             <Hero />
-        </>
+        </div>
     );
 };
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,18 +9,20 @@ const Hero = () => {
     return (
         <div className='hero'>
             <img className='hero__image' src={HeroImage} alt='Hero' />
-            <h1 className='hero__title'>
-                All your files in one secure location, accessible anywhere.
-            </h1>
-            <p className='hero__text'>
-                Fylo stores your most important files in one secure location.
-                Access them wherever you need, share and collaborate with
-                friends, and co-workers.
-            </p>
-            <Form
-                placeholder='Enter your email.'
-                buttonInnerText='Get Started'
-            />
+            <div className='hero__container'>
+                <h1 className='hero__title'>
+                    All your files in one secure location, accessible anywhere.
+                </h1>
+                <p className='hero__text'>
+                    Fylo stores your most important files in one secure
+                    location. Access them wherever you need, share and
+                    collaborate with friends, and co-workers.
+                </p>
+                <Form
+                    placeholder='Enter your email.'
+                    buttonInnerText='Get Started'
+                />
+            </div>
         </div>
     );
 };
