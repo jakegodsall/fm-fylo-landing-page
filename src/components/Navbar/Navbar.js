de3e93eeb0079db8b093adf5029df9eda8adee2e
jakegodsall <jake.edward.godsall@gmail.com>
2023-12-30 10:55:39 +0100

    fixed styling of Hero component in desktop

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -19,17 +19,38 @@ export default function NavBar({ mobileMenuOpen, setMobileMenuOpen }) {
                 )}
             </AnimatePresence>
 
-            {mobileMenuOpen ? (
-                <AiOutlineClose
-                    className={styles.navBar__hamburger}
-                    onClick={hamburgerHandler}
-                />
-            ) : (
-                <GiHamburgerMenu
-                    className={styles.navBar__hamburger}
-                    onClick={hamburgerHandler}
-                />
-            )}
+            <div className={styles.navBar__desktopMenu}>
+                <ul>
+                    <li>
+                        <a href="#/">
+                            <p>Features</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#/">
+                            <p>Team</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#/">
+                            <p>Sign In</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.navBar__mobileMenu}>
+                {mobileMenuOpen ? (
+                    <AiOutlineClose
+                        className={styles.navBar__hamburger}
+                        onClick={hamburgerHandler}
+                    />
+                ) : (
+                    <GiHamburgerMenu
+                        className={styles.navBar__hamburger}
+                        onClick={hamburgerHandler}
+                    />
+                )}
+            </div>
         </nav>
     );
 }
