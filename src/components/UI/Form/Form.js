40fc37a6f2a0be34b557941f8113dc63cac04541
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-01 09:01:08 +0100

    added email validation logic to Form component

--- src/components/UI/Form/Form.js
+++ src/components/UI/Form/Form.js
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+
+import validateEmail from "../../../utils/validators";
 
 import styles from "./Form.module.css";
 
 const Form = (props) => {
+    const [emailInput, setEmailInput] = useState("");
+    const [hasBeenEdited, setHasBeenEdited] = useState(false);
+
+    function onChangeHandler(event) {
+        setEmailInput(event.target.value);
+    }
+
+    function onBlurHandler(event) {
+        if (event.target.value === "") {
+            setHasBeenEdited(false);
+        } else {
+            setHasBeenEdited(true);
+        }
+    }
+
     function onClickHandler(event) {
         event.preventDefault();
     }
 
+    const emailIsInvalid = hasBeenEdited && !validateEmail(emailInput);
+
     return (
-        <form className={styles.form}>
-            <input
-                className={styles.form__input}
-                type="text"
-                placeholder={props.placeholder}
-                autoComplete="off"
-            />
-            <button
-                className={styles.form__submit}
-                type="submit"
-                onClick={onClickHandler}
-            >
-                {props.buttonInnerText}
-            </button>
-        </form>
+        <>
+            <form className={styles.form}>
+                <input
+                    className={styles.form__input}
+                    type="text"
+                    placeholder={props.placeholder}
+                    autoComplete="off"
+                    onChange={onChangeHandler}
+                    onBlur={onBlurHandler}
+                />
+                <button
+                    className={styles.form__submit}
+                    type="submit"
+                    onClick={onClickHandler}
+                >
+                    {props.buttonInnerText}
+                </button>
+            </form>
+            {emailIsInvalid && (
+                <p className={styles.form__validationMessage}>
+                    Email address is invalid
+                </p>
+            )}
+        </>
     );
 };
 
